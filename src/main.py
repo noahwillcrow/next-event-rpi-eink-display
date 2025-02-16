68616c1656e44df26b31f9a36be8b4eb91a0e07c
Noah Crowley <17803348+noahwillcrow@users.noreply.github.com>
2025-02-16 00:33:39 +0000

    add new feature to not show useless updates

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from artist import Artist
 from data_fetcher import DataFetcher
 from renderer import Renderer
 
+last_update_file_path = './last_update_data.gitignored.txt'
+
 
 def main():
     config = load_config_yaml('../config.yaml')
@@ -25,7 +27,8 @@ def main():
         small_text_size=int(config['image']['font']['small-text-size']),
     )
     if next_event is None:
-        image = artist.draw_text(config['image']['no-events-message'])
+        if did_last_update_have_event():
+            image = artist.draw_text(config['image']['no-events-message'])
     else:
         image = artist.draw_upcoming_event_notice(
             next_event.name, next_event.time_until_event
@@ -35,12 +38,30 @@ def main():
     renderer = Renderer(config['renderer'])
     renderer.render(image)
 
+    # save whether the last update had an event
+    set_did_last_update_have_event(next_event is not None)
+
+
+def did_last_update_have_event() -> bool:
+    """This function reads from a local file to see if the last update had an event."""
+    if not os.path.exists(last_update_file_path):
+        return True  # if the file doesn't exist, we assume that the last update had an event
+
+    with open(last_update_file_path, 'r') as file:
+        return file.read() == 'true'
+
 
 def load_config_yaml(config_path) -> dict:
     """This function reads the config yaml file and returns the content as a dictionary."""
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+
+def set_did_last_update_have_event(value: bool):
+    """This function writes whether the most recent update had an event to disk."""
+    with open(last_update_file_path, 'w') as file:
+        file.write(str(value))
 
 
 if __name__ == '__main__':
